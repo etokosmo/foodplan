6ea35137cef7680fd0e209101b0b908fef2ce7f5
Denis Polyakov <p-ov.da@ya.ru>
2022-10-06 20:39:06 +0300

    Add API and Recipe template

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -19,13 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts.views import HomePageView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recipes/', include('recipes.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('allauth.urls')),
     path('', HomePageView.as_view(), name='home')
-]
 
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
