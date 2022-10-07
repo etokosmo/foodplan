1b96a4f798e3d95e3b1a1841309ac1f91e1b8070
Alexander Samokhin <samokhin@shockland.ru>
2022-10-07 12:03:33 +0300

    Fix static

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media/')
 ]
 
+STATIC_ROOT = 'staticfiles/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
