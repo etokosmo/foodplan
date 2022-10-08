f0b2a10f399a64329c600f1ecf3ef47e314dba2c
Samokhin Alexander <icorwin74@gmail.com>
2022-10-08 20:31:36 +0300

    Add profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.templatetags.static import static
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    GENDER_MALE = 1
+    GENDER_FEMALE = 2
+    GENDER_CHOICES = [
+        (GENDER_MALE, ("Male")),
+        (GENDER_FEMALE, ("Female")),
+    ]
+
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES, null=True, blank=True)
+    phone = models.CharField(max_length=32, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    number = models.CharField(max_length=32, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    zip = models.CharField(max_length=30, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = ('Profile')
+        verbose_name_plural = ('Profiles')
+
+    @property
+    def get_avatar(self):
+        return self.avatar.url if self.avatar else static('test_avatar.png')
