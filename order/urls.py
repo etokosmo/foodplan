439f28a4b768be48b8c4cb6aea651691bc036c97
Denis Polyakov <p-ov.da@ya.ru>
2022-10-09 22:23:02 +0300

    Add payment

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import create_order
+from .views import create_order, success_payment
 
 app_name = "order"
 
 urlpatterns = [
     path('create/', create_order, name='create_order'),
+    path('success_payment/', success_payment, name='success_payment'),
 ]
